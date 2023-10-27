e3b62c25a5baea53c4768727d2e7c7987a37c1b2
Mohammad-Abbasi <mohammad.abasy.79@gmail.com>
2023-10-27 11:29:56 -0700

    feat(UnitOfWork): Add disposable to UnitOfWork

--- project/src/BLRazor/Connection/UnitOfWork.cs
+++ project/src/BLRazor/Connection/UnitOfWork.cs
@@ -3,11 +3,34 @@ using Data.Repository;
 
 namespace BLRazor.Connection;
 
-public class UnitOfWork : IUnitOfWork
+public class UnitOfWork : IDisposable, IUnitOfWork
 {
     private readonly ConsoleContext context;
 
+    private bool disposed = false;
+
     public UnitOfWork() => context = new();
+
+    ~UnitOfWork() => Dispose(false);
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposed)
+        {
+            if (disposing)
+            {
+                context.Dispose();
+            }
+
+            disposed = true;
+        }
+    }
 
     public IRepositoryBase<TEntity> RepositoryBase<TEntity>() where TEntity : class => new RepositoryBase<TEntity>(context);
 
